48264ebbfaebfedce3fbef4739ed688220a0575c
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-04-20 14:44:36 -0300

    inserindo entidade para o Admin

---
+++ root/api/src/modules/admin/entities/Admin.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('admins')
+export class Admin {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', unique: true, nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  password: string;
+
+  @Column({ name: 'created_at' })
+  createdAt: Date;
+
+  @Column({ name: 'updated_at' })
+  updatedAt: Date;
+}
+
+export default Admin;
