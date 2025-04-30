004fb1b95104c0bc7d8f57b3cd8aa25637ef61f1
Letícia <leticiabandeiralucena@gmail.com>
2025-04-30 19:41:31 -0300

    adicionando tabela doctors

---
+++ root/api/src/infra/database/migrations/1746052045216-CreateDoctorTable.ts
@@ -0,0 +1,66 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
+
+export class CreateDoctorTable1746052045216 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'doctors',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'crm',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_by_admin_id',
+            type: 'uuid',
+            isNullable: false,
+
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey("doctors", new TableForeignKey({columnNames:["created_by_admin_id"], referencedColumnNames:["uui"], referencedTableName:"admins", onDelete:"CASCADE"}));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('doctors');
+  }
+}
