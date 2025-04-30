01a20ae7f3d80edcc351afde8eebdfba760190c2
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-04-30 14:22:44 -0300

    atualizando o layout do componente de login do médico e ajustando estilos de fonte; adicionando cor de fundo ao body

--- root/frontend/src/Pages/DoctorPages/DoctorLogin.jsx
+++ root/frontend/src/Pages/DoctorPages/DoctorLogin.jsx
@@ -5,25 +5,43 @@ import LogoIcon from '../../Assets/LogoIcon.svg?react';
 export default function DoctorLogin() {
   return (
     <div className="">
-      <div className="title">
-        <LogoIcon />
-        <h1>Login como médico</h1>
+      <div className="mb-8">
+        <div className="">
+          <LogoIcon />
+        </div>
+        <h1 className="font-poppins text-gray-12 text-2xl font-normal">
+          Login como médico
+        </h1>
       </div>
       <div className="form">
         <form action="">
-          <label htmlFor="">Username ou E-mail</label>
+          <label
+            className="font-roboto text-gray-12 text-sm font-normal"
+            htmlFor=""
+          >
+            Username ou E-mail
+          </label>
           <input type="email" placeholder="exemplo@email.com" />
-          <label htmlFor="">Senha</label>
+          <label
+            className="font-roboto text-gray-12 text-sm font-normal"
+            htmlFor=""
+          >
+            Senha
+          </label>
           <input type="password" />
-          <a href="#">Esqueci minha senha</a>
+          <a className="font-roboto text-gray-11 text-sm font-normal" href="#">
+            Esqueci minha senha
+          </a>
           <button type="submit">Entrar</button>
         </form>
       </div>
       <div className="singin">
-        <p className="mb-2">Não tem uma conta do AlzCheck?</p>
+        <p className="font-roboto text-gray-12 text-sm font-normal">
+          Não tem uma conta do AlzCheck?
+        </p>
         <button>Crie uma nova conta</button>
       </div>
-      <p>
+      <p className="font-roboto text-gray-12 text-sm font-normal">
         © AlzCheck, 2025. <a href="#">Termos</a>.{' '}
         <a href="#">Política de privacidade</a>.
       </p>
