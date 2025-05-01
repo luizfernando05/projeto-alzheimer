b5707f3c494c509233f2f6727f9572780a3e7189
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-05-01 11:05:18 -0300

    inserido rota de update

--- root/api/src/modules/doctor/routes/doctor.routes.ts
+++ root/api/src/modules/doctor/routes/doctor.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { CreateDoctorController } from '../controllers/CreateDoctorController';
 import { LoginDoctorController } from '../controllers/LoginDoctorController';
+import { UpdateDoctorController } from '../controllers/UpdateDoctorController';
 
 const doctorRoutes = Router();
 const createDoctorController = new CreateDoctorController();
 const loginDoctorController = new LoginDoctorController();
+const updateDoctorController = new UpdateDoctorController();
 
 doctorRoutes.post('/', (req, res, next) => {
   createDoctorController.handle(req, res, next);
@@ -12,6 +14,10 @@ doctorRoutes.post('/', (req, res, next) => {
 
 doctorRoutes.post('/login', (req, res, next) => {
   loginDoctorController.handle(req, res, next);
+});
+
+doctorRoutes.put('/:id', (req, res, next) => {
+  updateDoctorController.handle(req, res, next);
 });
 
 export { doctorRoutes };
