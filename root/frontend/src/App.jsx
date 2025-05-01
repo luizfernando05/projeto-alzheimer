24e6f1ac1af7436f4a91487afaad0b53e0e44a20
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-05-01 13:06:21 -0300

    adicionando componente DoctorSingin e rota correspondente ao aplicativo

--- root/frontend/src/App.jsx
+++ root/frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import DoctorLogin from './Pages/DoctorPages/DoctorLogin';
+import DoctorSingin from './Pages/DoctorPages/DoctorSingin';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Route path="/" element={<Navigate to="/login/doctor" />} />{' '}
       {/* por enquanto, ir para a rota de login ao acessar '/' */}
       <Route path="/login/doctor" element={<DoctorLogin />} />
+      <Route path="/singin/doctor" element={<DoctorSingin />} />
     </Routes>
   );
 }
---
+++ root/frontend/src/Pages/DoctorPages/DoctorSingin.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DoctorSingin() {
+  return <div>DoctorSingin</div>;
+}
+
+export default DoctorSingin;
