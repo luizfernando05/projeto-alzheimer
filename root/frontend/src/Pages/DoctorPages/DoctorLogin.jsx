84b1e64d347c9ac5f21757bb98abefbe22dd2b9d
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-05-01 11:48:12 -0300

    adicionando suporte para ícones de senha e ocultação no login do médico

--- root/frontend/src/Pages/DoctorPages/DoctorLogin.jsx
+++ root/frontend/src/Pages/DoctorPages/DoctorLogin.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
-import { indigo, gray } from '@radix-ui/colors';
 import LogoIcon from '../../Assets/LogoIcon.svg?react';
+import { Eye, EyeSlash } from '@phosphor-icons/react';
 
 export default function DoctorLogin() {
+  const [showPasword, setShowPassword] = useState(false);
+
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="flex flex-col items-center w-lg space-y-6 p-8 ">
+      <div className="flex flex-col items-center space-y-6 p-8 ">
         <div className="mb-8">
           <div className="flex flex-col items-center space-y-4 pb-4">
             <LogoIcon />
@@ -27,7 +29,7 @@ export default function DoctorLogin() {
               Username ou E-mail
             </label>
             <input
-              className="rounded-sm border border-gray-06 bg-gray-02 p-3 font-roboto text-gray-12 text-sm font-normal"
+              className="rounded-sm border border-gray-06 bg-gray-02 p-3 font-roboto text-gray-12 text-sm font-normal focus:outline-indigo-07"
               type="email"
               placeholder="exemplo@email.com"
             />
@@ -40,12 +42,23 @@ export default function DoctorLogin() {
             >
               Senha
             </label>
-            <input
-              className="rounded-sm border border-gray-06 bg-gray-02 p-3 font-roboto text-gray-12 text-sm font-normal"
-              type="password"
-            />
+
+            <div className="relative">
+              <input
+                type={showPasword ? 'text' : 'password'}
+                className="w-full rounded-sm border border-gray-06 bg-gray-02 p-3 pr-10 font-roboto text-gray-12 text-sm font-normal focus:outline-indigo-07"
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPasword)}
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-11"
+              >
+                {showPasword ? <EyeSlash size={16} /> : <Eye size={16} />}
+              </button>
+            </div>
+
             <a
-              className="font-roboto text-gray-11 text-sm font-normal"
+              className="self-end font-roboto text-gray-11 text-xs font-normal hover:underline"
               href="#"
             >
               Esqueci minha senha
@@ -54,7 +67,7 @@ export default function DoctorLogin() {
 
           <button
             type="submit"
-            className="bg-indigo-09 text-gray-01 rounded py-2 font-roboto text-base font-normal hover:bg-indigo-10 transition"
+            className="bg-indigo-09 text-gray-01 rounded py-2 font-roboto text-base font-normal hover:bg-indigo-10 transition shadow-xs"
           >
             Entrar
           </button>
@@ -64,18 +77,18 @@ export default function DoctorLogin() {
           <p className="font-roboto text-gray-12 text-sm font-normal">
             Não tem uma conta do AlzCheck?
           </p>
-          <button className="font-roboto text-gray-12 text-sm font-normal mt-2 border border-gray-5 rounded py-2 px-4 hover:bg-gray-2">
+          <button className="font-roboto text-gray-12 text-sm font-normal mt-2 border border-gray-06 rounded py-2 px-4 hover:bg-gray-2 pr-28 pl-28 bg-gray-02 hover:bg-gray-03 transition shadow-xs">
             Crie uma nova conta
           </button>
         </div>
 
-        <p className="font-roboto text-gray-11 text-sm font-normal text-center">
+        <p className="font-roboto text-gray-11 text-xs font-normal text-center">
           © AlzCheck, 2025.{' '}
-          <a href="#" className="underline">
+          <a href="#" className="hover:text-indigo-12">
             Termos
           </a>
           .{' '}
-          <a href="#" className="underline">
+          <a href="#" className="hover:text-indigo-12">
             Política de privacidade
           </a>
           .
