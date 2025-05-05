24b4e459f8b70aebc1e09a5dfd542c982d09ebe0
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-05-05 13:26:06 -0300

    adicionando validações para username e celular no CreateDoctorValidator

--- root/api/src/modules/doctor/validations/CreateDoctorValidator.ts
+++ root/api/src/modules/doctor/validations/CreateDoctorValidator.ts
@@ -11,4 +11,12 @@ export const CreateDoctorValidator = yup.object().shape({
     .string()
     .min(6, 'Password must be at least 6 characters long')
     .required('Password is required'),
+  username: yup.string().required('Username is required'),
+  celphone: yup
+    .string()
+    .required('Cellphone is required')
+    .matches(
+      /^(\(\d{2}\)\s?\d{5}-\d{4})$/,
+      'Invalid format. Please enter in the format (xx)xxxxx-xxxx'
+    ),
 });
