8a4b2a3af847c5165972c90aa55fe6b4510ed55a
Letícia <leticiabandeiralucena@gmail.com>
2025-06-07 16:14:50 -0300

    migration do paciente

---
+++ root/api/src/infra/database/migrations/1749322515666-CreatePatientTable.ts
@@ -0,0 +1,88 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreatePatientTable1749322515666 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'patients',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'birth_date',
+            type: 'date',
+            isNullable: false,
+          },
+          {
+            name: 'gender',
+            type: 'char',
+            isNullable: false,
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+
+          {
+            name: 'created_by_doctor_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'patients',
+      new TableForeignKey({
+        columnNames: ['created_by_doctor_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'doctors',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('patients');
+  }
+}
