31a51e5156537e637849fe66598fd2331e8a4cc1
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-06-10 14:05:39 -0300

    alterando tabela de paciente

---
+++ root/api/src/infra/database/migrations/1749574632740-AlterPatientTable.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class AlterPatientTable1749574632740 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'patients',
+      new TableColumn({
+        name: 'ethnicity',
+        type: 'varchar',
+        isNullable: true,
+      })
+    );
+
+    await queryRunner.addColumn(
+      'patients',
+      new TableColumn({
+        name: 'educational_level',
+        type: 'varchar',
+        isNullable: true,
+      })
+    );
+
+    await queryRunner.addColumn(
+      'patients',
+      new TableColumn({
+        name: 'phone_number',
+        type: 'varchar',
+        isNullable: true,
+      })
+    );
+
+    await queryRunner.addColumn(
+      'patients',
+      new TableColumn({
+        name: 'selfie_photo',
+        type: 'varchar',
+        isNullable: true,
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
