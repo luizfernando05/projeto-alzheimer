637573a1fd82613d67042dddc1780322a2412e63
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-06-11 14:27:22 -0300

    corrigindo parâmetro da função create na interface IPatientRepository e implementando a classe PatientRepository

--- root/api/src/modules/patient/interfaces/IPatientRepository.ts
+++ root/api/src/modules/patient/interfaces/IPatientRepository.ts
@@ -1,11 +1,10 @@
 import Patient from '../../../domain/entities/Patient';
 
 export interface IPatientRepository {
-  create(doctor: Patient): Promise<Patient>;
+  create(patient: Patient): Promise<Patient>;
   findByEmail(email: string): Promise<Patient | null>;
   findById(id: string): Promise<Patient | null>;
   findAll(): Promise<Patient[]>;
-  findByUsername(username: string): Promise<Patient | null>;
-  findByCelphone(celphone: string): Promise<Patient | null>;
+  findByCelphone(phoneNumber: string): Promise<Patient | null>;
   update(patient: Patient): Promise<Patient>;
 }
---
+++ root/api/src/modules/patient/repositories/PatientRepository.ts
@@ -0,0 +1,37 @@
+import { Repository } from 'typeorm';
+import { AppDataSource } from '../../../config/data-source';
+import Patient from '../../../domain/entities/Patient';
+import { IPatientRepository } from '../interfaces/IPatientRepository';
+
+export class PatientRepository implements IPatientRepository {
+  private ormRepository: Repository<Patient>;
+
+  constructor() {
+    this.ormRepository = AppDataSource.getRepository(Patient);
+  }
+
+  async create(patient: Patient): Promise<Patient> {
+    const createPatient = this.ormRepository.create(patient);
+    return this.ormRepository.save(createPatient);
+  }
+
+  async findByEmail(email: string): Promise<Patient | null> {
+    return this.ormRepository.findOne({ where: { email } });
+  }
+
+  async findById(id: string): Promise<Patient | null> {
+    return this.ormRepository.findOne({ where: { id } });
+  }
+
+  async findAll(): Promise<Patient[]> {
+    return this.ormRepository.find();
+  }
+
+  async findByCelphone(phoneNumber: string): Promise<Patient | null> {
+    return this.ormRepository.findOne({ where: { phoneNumber } });
+  }
+
+  async update(patient: Patient): Promise<Patient> {
+    return this.ormRepository.save(patient);
+  }
+}
