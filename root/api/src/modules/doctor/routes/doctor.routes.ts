e842fe722bf25bc206b53315172d0136d456967c
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-06-20 13:47:44 -0300

    adicionando rota para obter perfil do médico com autenticação

--- root/api/src/modules/doctor/routes/doctor.routes.ts
+++ root/api/src/modules/doctor/routes/doctor.routes.ts
@@ -5,6 +5,9 @@ import { LoginDoctorController } from '../controllers/LoginDoctorController';
 import { UpdateDoctorController } from '../controllers/UpdateDoctorController';
 import { upload } from '../../../config/multer.config';
 import ListDoctorController from '../controllers/ListDoctorController';
+import { ensureDoctorAuthenticated } from '../../shared/middlewares/ensureDoctorAuthenticated';
+import DoctorRepository from '../repositories/DoctorRepository';
+import { AppError } from '../../shared/errors/AppError';
 
 const doctorRoutes = Router();
 const createDoctorController = new CreateDoctorController();
@@ -34,5 +37,28 @@ doctorRoutes.put('/:id', (req, res, next) => {
 doctorRoutes.get('/list', (req, res, next) => {
   listDoctorController.handle(req, res, next);
 });
+
+doctorRoutes.get(
+  '/profile',
+  ensureDoctorAuthenticated,
+  async (req, res, next) => {
+    try {
+      const doctorRepository = new DoctorRepository();
+      const doctor = await doctorRepository.findById(req.user.id);
+
+      if (!doctor) {
+        return next(new AppError('Médico não encontrado', 404));
+      }
+
+      res.status(200).json({
+        id: doctor.id,
+        name: doctor.name,
+        email: doctor.email,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 export { doctorRoutes };
