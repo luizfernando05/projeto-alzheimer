eb38a27d9e9f27f8c31330cc31e5a1025fed9487
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-06-20 11:53:54 -0300

    refatorando DoctorLayout para adicionar referência ao menu rápido e melhorar a estrutura do componente

--- root/frontend/src/Pages/DoctorPages/DoctorLayout.jsx
+++ root/frontend/src/Pages/DoctorPages/DoctorLayout.jsx
@@ -1,14 +1,22 @@
+import React, { useRef } from 'react';
 import DoctorHeader from '../../Components/Header/DoctorHeader';
+import DoctorQuickMenu from '../../Components/QuickMenu/DoctorQuickMenu';
 import DoctorSidebar from '../../Components/Sidebar/DoctorSidebar';
 
 const DoctorLayout = ({ children }) => {
+  const mainRef = useRef(null);
+
   return (
     <div className="flex h-screen w-screen">
       <DoctorSidebar />
       <div className="flex-1 flex flex-col">
         <DoctorHeader />
-        <main className="flex-1 px-8 py-6 bg-gray-02 overflow-y-auto">
+        <main
+          ref={mainRef}
+          className="relative flex-1 px-8 py-6 bg-gray-02 overflow-y-auto"
+        >
           {children}
+          <DoctorQuickMenu scrollRef={mainRef} />
         </main>
       </div>
     </div>
