0680f7fd941518562cfce1694445815cbe264d5b
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-06-20 11:56:02 -0300

    adicionando novos ícones e tooltip ao cabeçalho do médico, incluindo opções de perfil e logout

--- root/frontend/src/Components/Header/DoctorHeader.jsx
+++ root/frontend/src/Components/Header/DoctorHeader.jsx
@@ -1,4 +1,11 @@
-import { Bell, Gear, MagnifyingGlass, Sun } from '@phosphor-icons/react';
+import {
+  Bell,
+  Gear,
+  MagnifyingGlass,
+  User,
+  Wind,
+  SignOut,
+} from '@phosphor-icons/react';
 import DoctorProfile from '../../Assets/DoctorProfile.svg?react';
 import React from 'react';
 
@@ -18,17 +25,34 @@ const DoctorHeader = () => {
       </div>
 
       <div className="flex items-center gap-4 text-gray-11">
-        <button className="hover:text-gray-12 transition">
+        <button className="hover:text-gray-12 transition relative group cursor-pointer">
           <Bell size={24} />
+          <div className="absolute right-0 mt-2 w-40 bg-gray-02/70 shadow-lg border border-gray-06 rounded-md opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity duration-200 z-50">
+            <div className="flex flex-col gap-3 items-center text-gray-11 m-3">
+              <Wind size={32} />
+              <span className="font-roboto font-normal text-xs">
+                Tudo tranquilo por aqui!
+              </span>
+            </div>
+          </div>
         </button>
         <button className="hover:text-gray-12 transition">
           <Gear size={24} />
         </button>
-        <button className="hover:text-gray-12 transition">
-          <Sun size={24} />
-        </button>
-        <button className="border border-indigo-09 rounded-full p-1 hover:border-indigo-12 transition">
+        <button className="border border-indigo-09 rounded-full p-1 hover:border-indigo-12 transition relative group cursor-pointer">
           <DoctorProfile size={24} />
+          <div className="absolute right-0 mt-2 w-40 bg-gray-02/70 shadow-lg border border-gray-06 rounded-md opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity duration-200 z-50">
+            <ul className="flex flex-col font-roboto text-sm text-gray-11">
+              <li className="flex gap-3 items-center px-4 py-2 hover:bg-gray-04 hover:text-gray-12 cursor-pointer">
+                <User size={14} />
+                Ver Perfil
+              </li>
+              <li className="flex gap-3 items-center px-4 py-2 hover:bg-red-100 hover:text-red-950 cursor-pointer">
+                <SignOut size={14} />
+                Sair
+              </li>
+            </ul>
+          </div>
         </button>
       </div>
     </header>
