2878369bb51872b59c6c8a609ced8d7b7f035870
Letícia <leticiabandeiralucena@gmail.com>
2025-06-21 03:47:55 -0300

    adicionando as rotas para criação de dados médicos com autenticação de médico

---
+++ root/api/src/modules/medicalData/routes/medicalData.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { ensureDoctorAuthenticated } from '../../shared/middlewares/ensureDoctorAuthenticated';
+import CreateMedicalDataController from '../controllers/CreateMedicalDataController';
+
+const medicalDataRoutes = Router();
+const createMedicalDataController = new CreateMedicalDataController();
+
+medicalDataRoutes.post(
+  '/',
+
+  ensureDoctorAuthenticated,
+
+  (req, res, next) => {
+    createMedicalDataController.handle(req, res, next);
+  }
+);
+
+export { medicalDataRoutes };
