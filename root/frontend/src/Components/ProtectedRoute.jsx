d01700d0b4d2f97b43cc65fc4556d29ed8f40cdf
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-06-21 15:34:06 -0300

    ajustando a mensagem de carregamento no ProtectedRoute para usar um loader estilizado

--- root/frontend/src/Components/ProtectedRoute.jsx
+++ root/frontend/src/Components/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import { useAuth } from '../Hooks/DoctorHooks/useAuth';
 import { Navigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 export function ProtectedRoute({ children }) {
   const { doctor, loading } = useAuth();
 
   if (loading) {
-    return <div>Carregando...</div>;
+    return <div className="loader"></div>;
   }
 
   if (!doctor) {
