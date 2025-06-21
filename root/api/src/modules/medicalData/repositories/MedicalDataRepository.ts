4ccff38b57d1c391c4e4ec16dfa02c9c99684201
Letícia <leticiabandeiralucena@gmail.com>
2025-06-21 02:14:33 -0300

    adicionando a classe MedicalDataRepository para gerenciamento de dados médicos

---
+++ root/api/src/modules/medicalData/repositories/MedicalDataRepository.ts
@@ -0,0 +1,29 @@
+import { Repository } from 'typeorm';
+import { AppDataSource } from '../../../config/data-source';
+import MedicalData from '../../../domain/entities/MedicalData';
+import { IMedicalDataRepository } from '../interfaces/IMedicalDataRepository';
+
+export class MedicalDataRepository implements IMedicalDataRepository {
+  private ormRepository: Repository<MedicalData>;
+
+  constructor() {
+    this.ormRepository = AppDataSource.getRepository(MedicalData);
+  }
+
+  async create(medicalData: MedicalData): Promise<MedicalData> {
+    const createMedicalData = this.ormRepository.create(medicalData);
+    return this.ormRepository.save(createMedicalData);
+  }
+
+  async findById(id: string): Promise<MedicalData | null> {
+    return this.ormRepository.findOne({ where: { id } });
+  }
+
+  async findAll(): Promise<MedicalData[]> {
+    return this.ormRepository.find();
+  }
+
+  async update(medicalData: MedicalData): Promise<MedicalData> {
+    return this.ormRepository.save(medicalData);
+  }
+}
