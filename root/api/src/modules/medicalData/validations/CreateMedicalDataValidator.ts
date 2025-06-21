5ba019288d8205e7a84f9caaf5d26546f22ecf60
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-06-21 12:31:36 -0300

    corrigindo a nomenclatura das propriedades de validação no CreateMedicalDataValidator

--- root/api/src/modules/medicalData/validations/CreateMedicalDataValidator.ts
+++ root/api/src/modules/medicalData/validations/CreateMedicalDataValidator.ts
@@ -2,20 +2,20 @@ import * as yup from 'yup';
 
 export const CreateMedicalDataValidator = yup.object().shape({
   bmi: yup.string().required('IMC é obrigatório'),
-  sleep_quality: yup.string().required('Qualidade de sono é obrigatório'),
-  cholesterol_ldl: yup.string().required('Colesterol LDL é obrigatório'),
-  cholesterol_hdl: yup.string().required('Colesterol HDL é obrigatório'),
-  cholesterol_triglycerides: yup
+  sleepQuality: yup.string().required('Qualidade de sono é obrigatório'),
+  cholesterolLdl: yup.string().required('Colesterol LDL é obrigatório'),
+  cholesterolHdl: yup.string().required('Colesterol HDL é obrigatório'),
+  cholesterolTriglycerides: yup
     .string()
     .required('Colesterol triglicerideos é obrigatório'),
   mmse: yup.string().required('Teste MMSE é obrigatório'),
-  functional_assessment: yup
+  functionalAssessment: yup
     .string()
     .required('Avaliação funcional é obrigatório'),
-  memory_complaints: yup.string().required('Queixas de memória é obrigatório'),
-  behavioral_problems: yup
+  memoryComplaints: yup.string().required('Queixas de memória é obrigatório'),
+  behavioralProblems: yup
     .string()
     .required('Problemas comportamentais é obrigatório'),
   adl: yup.string().required('ADL é obrigatório'),
-  date_exam: yup.string().required('Data é obrigatório'),
+  dateExam: yup.string().required('Data é obrigatório'),
 });
