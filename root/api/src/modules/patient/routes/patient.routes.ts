80d53cbe0801de0fb3726968ce6d3005aed472a1
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-06-26 14:56:52 -0300

    adicionando rota para obter pacientes dos últimos 7 dias

--- root/api/src/modules/patient/routes/patient.routes.ts
+++ root/api/src/modules/patient/routes/patient.routes.ts
@@ -5,12 +5,14 @@ import { ensureDoctorAuthenticated } from '../../shared/middlewares/ensureDoctor
 import { ListPatientsByDoctorController } from '../controllers/ListPatientsByDoctorController';
 import { CountPatientsByDoctorController } from '../controllers/CountPatientsByDoctorController';
 import { GetPatientSummaryController } from '../controllers/GetPatientSummaryController';
+import GetPatientsByLast7DaysController from '../controllers/GetPatientsByLast7DaysController';
 
 const patientRoutes = Router();
 const createPatientController = new CreatePatientController();
 const listPatientsByDoctorController = new ListPatientsByDoctorController();
 const countPatientsByDoctorController = new CountPatientsByDoctorController();
 const getPatientSummaryController = new GetPatientSummaryController();
+const getPatientsByLast7DaysController = new GetPatientsByLast7DaysController();
 
 patientRoutes.post(
   '/',
@@ -36,5 +38,13 @@ patientRoutes.get('/count', ensureDoctorAuthenticated, (req, res, next) => {
 patientRoutes.get('/summary', ensureDoctorAuthenticated, (req, res, next) => {
   getPatientSummaryController.handle(req, res, next);
 });
+
+patientRoutes.get(
+  '/last-7-days',
+  ensureDoctorAuthenticated,
+  (req, res, next) => {
+    getPatientsByLast7DaysController.handle(req, res, next);
+  }
+);
 
 export { patientRoutes };
