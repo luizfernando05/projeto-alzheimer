a64b4c9dafc81e163f45ecc0c30c0f61e2ca71ef
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-06-26 14:24:04 -0300

    adicionando exibição de crescimento no total de pacientes

--- root/frontend/src/Pages/DoctorPages/PatientModulePages/MainModule.jsx
+++ root/frontend/src/Pages/DoctorPages/PatientModulePages/MainModule.jsx
@@ -8,24 +8,29 @@ import {
   Pencil,
   Plus,
   Sparkle,
+  TrendDown,
+  TrendUp,
   Users,
   UsersFour,
 } from '@phosphor-icons/react';
 
 const MainModule = () => {
   const [totalPatients, setTotalPatients] = useState(0);
+  const [growth, setGrowth] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPatientCount = async () => {
       try {
         const response = await axios.get(
-          'http://localhost:3001/patient/count',
+          'http://localhost:3001/patient/summary',
           {
             withCredentials: true,
           }
         );
+
         setTotalPatients(response.data.total);
+        setGrowth(response.data.growth);
       } catch (error) {
         console.error('Erro ao buscar total de pacientes:', error);
       } finally {
@@ -51,13 +56,13 @@ const MainModule = () => {
 
           <div className="pt-6 pr-8 pl-8">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-5">
-              <div className="rounded-lg border border-gray-06 overflow-hidden">
-                <div className="flex bg-gray-02 gap-6 pt-5 pb-5 pr-6 pl-6 border-b border-gray-06">
+              <div className="bg-gray-02 rounded-lg border border-gray-06 overflow-hidden">
+                <div className="flex gap-6 pt-5 pb-5 pr-6 pl-6 border-b border-gray-06">
                   <h2 className="text-base font-poppins font-normal text-gray-12">
                     Opções
                   </h2>
                 </div>
-                <div className="bg-gray-02 pt-4 pb-5 pr-6 pl-6 flex flex-wrap gap-3">
+                <div className="pt-4 pb-5 pr-6 pl-6 flex flex-wrap gap-3">
                   <button className="cursor-pointer bg-gray-01 flex gap-2.5 items-center font-roboto text-sm text-gray-11 font-normal rounded-sm border border-gray-06 pt-1 pb-1 pl-2 pr-2 hover:bg-gray-03">
                     <Plus size={12} />
                     <span className="text-gray-12">Adicionar paciente</span>
@@ -89,8 +94,8 @@ const MainModule = () => {
                 </div>
               </div>
 
-              <div className="rounded-lg border border-gray-06 overflow-hidden">
-                <div className="flex bg-gray-02 gap-6 pt-5 pb-5 pr-6 pl-6 border-b border-gray-06">
+              <div className="bg-gray-02 rounded-lg border border-gray-06 overflow-hidden">
+                <div className="flex gap-6 pt-5 pb-5 pr-6 pl-6 border-b border-gray-06">
                   <h2 className="text-base font-poppins font-normal text-gray-12 flex items-center gap-3">
                     <div className="border border-gray-06 p-1 rounded-sm">
                       <Users size={16} />
@@ -98,10 +103,31 @@ const MainModule = () => {
                     Total de Pacientes
                   </h2>
                 </div>
-                <div className="bg-gray-02 pt-4 pb-5 pr-6 pl-6">
-                  <h2 className="text-xl font-roboto font-normal text-gray-12">
-                    {loading ? '...' : totalPatients.toLocaleString('pt-br')}
-                  </h2>
+                <div className="pt-4 pb-5 pr-6 pl-6">
+                  <div className="flex gap-4">
+                    <h2 className="text-xl font-roboto font-normal text-gray-12">
+                      {loading ? '...' : totalPatients.toLocaleString('pt-br')}
+                    </h2>
+                    {!loading && growth && (
+                      <span
+                        className={`font-roboto text-xs font-medium border border-gray-06 p-1 rounded-sm ${
+                          growth.percentage >= 0
+                            ? 'text-green-600'
+                            : 'text-red-600'
+                        } flex items-center gap-1`}
+                      >
+                        {growth.percentage >= 0 ? <TrendUp /> : <TrendDown />}
+                        {Math.abs(growth.percentage).toFixed(2)}%
+                      </span>
+                    )}
+                  </div>
+                  {!loading && growth && (
+                    <p className="font-roboto text-xs text-gray-11 mt-3">
+                      O número de pacientes{' '}
+                      {growth.percentage >= 0 ? 'aumentou' : 'diminuiu'} nos
+                      últimos 7 dias.
+                    </p>
+                  )}
                 </div>
               </div>
             </div>
