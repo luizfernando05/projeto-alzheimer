3661c47af64e2a8251c7822d9f1570818002815f
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-06-26 11:24:46 -0300

    adicionando rota para listar pacientes por ID do médico

--- root/api/src/modules/patient/routes/patient.routes.ts
+++ root/api/src/modules/patient/routes/patient.routes.ts
@@ -2,18 +2,26 @@ import { Router } from 'express';
 import { upload } from '../../../config/multer.config';
 import CreatePatientController from '../controllers/CreatePatientController';
 import { ensureDoctorAuthenticated } from '../../shared/middlewares/ensureDoctorAuthenticated';
+import { ListPatientsByDoctorController } from '../controllers/ListPatientsByDoctorController';
 
 const patientRoutes = Router();
 const createPatientController = new CreatePatientController();
+const listPatientsByDoctorController = new ListPatientsByDoctorController();
 
 patientRoutes.post(
   '/',
-
   ensureDoctorAuthenticated,
-
   upload.fields([{ name: 'selfiePhoto', maxCount: 1 }]),
   (req, res, next) => {
     createPatientController.handle(req, res, next);
+  }
+);
+
+patientRoutes.get(
+  '/doctors/list',
+  ensureDoctorAuthenticated,
+  (req, res, next) => {
+    listPatientsByDoctorController.hanlde(req, res, next);
   }
 );
 
