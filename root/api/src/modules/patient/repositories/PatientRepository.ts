5d155740e3293fc95abd40b4f5a6e705415cf49e
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-06-26 14:56:13 -0300

    adicionando método para obter contagem de pacientes nos últimos 7 dias

--- root/api/src/modules/patient/repositories/PatientRepository.ts
+++ root/api/src/modules/patient/repositories/PatientRepository.ts
@@ -2,12 +2,47 @@ import { Between, Repository } from 'typeorm';
 import { AppDataSource } from '../../../config/data-source';
 import Patient from '../../../domain/entities/Patient';
 import { IPatientRepository } from '../interfaces/IPatientRepository';
+import { startOfDay, subDays, addDays } from 'date-fns';
 
 export class PatientRepository implements IPatientRepository {
   private ormRepository: Repository<Patient>;
 
   constructor() {
     this.ormRepository = AppDataSource.getRepository(Patient);
+  }
+
+  async getCountByDateLast7Days(
+    doctorId: string
+  ): Promise<{ date: string; count: number }[]> {
+    const today = startOfDay(new Date());
+    const sevenDaysAgo = subDays(today, 6);
+
+    const queryResult = await this.ormRepository
+      .createQueryBuilder('patient')
+      .select("TO_CHAR(patient.createdAt, 'YYYY-MM-DD')", 'date')
+      .addSelect('COUNT(*)', 'count')
+      .where('patient.created_by_doctor_id = :doctorId', { doctorId })
+      .andWhere('patient.createdAt BETWEEN :start AND :end', {
+        start: sevenDaysAgo.toISOString(),
+        end: addDays(today, 1).toISOString(),
+      })
+      .groupBy('date')
+      .orderBy('date', 'ASC')
+      .getRawMany();
+
+    const fullResult: { date: string; count: number }[] = [];
+    for (let i = 0; i < 7; i++) {
+      const date = subDays(today, 6 - i)
+        .toISOString()
+        .slice(0, 10);
+      const match = queryResult.find((d) => d.date === date);
+      fullResult.push({
+        date,
+        count: match ? Number(match.count) : 0,
+      });
+    }
+
+    return fullResult;
   }
 
   async countByDoctorId(doctorId: string): Promise<number> {
