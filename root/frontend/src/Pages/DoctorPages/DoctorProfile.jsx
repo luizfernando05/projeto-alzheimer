bc0ad24aa681e237baa315583bb142b75e9517d3
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-06-30 12:03:50 -0300

    adicionando funcionalidade de perfil do médico com carregamento de dados da API

--- root/frontend/src/Pages/DoctorPages/DoctorProfile.jsx
+++ root/frontend/src/Pages/DoctorPages/DoctorProfile.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DoctorLayout from './DoctorLayout';
 import { ArrowLeft } from '@phosphor-icons/react';
+import axios from 'axios';
+import InputField from '../../Components/Form/InputField';
 
 const DoctorProfile = () => {
+  const [doctor, setDoctor] = useState(null);
+  const [form, setForm] = useState({});
+  const [loading, setLoading] = useState(true);
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    axios
+      .get(`${apiUrl}/doctor/get/data`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setDoctor(res.data);
+        setForm(res.data);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const handleChange = (e) => {
+    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const hanndleCancel = () => [setForm(doctor)];
+
+  const handleSubmit = (e) => {
+    e.preventeDefault();
+    console.log('Enviar para api');
+  };
+
+  if (loading) return <p>Carregando...</p>;
+
   return (
     <DoctorLayout>
       <section className="w-full">
@@ -21,7 +54,45 @@ const DoctorProfile = () => {
               </h2>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-3"></div>
+            <form onSubmit={handleSubmit} className="pt-6 pr-8 pl-8">
+              <div className="flex items-stretch gap-x-6 flex-wrap md:flex-nowrap mb-4">
+                <img
+                  src={`${apiUrl}/${doctor.selfiePhoto?.replace(/\\/g, '/')}`}
+                  alt="Foto do médico"
+                  className="h-19 w-auto aspect-square border border-indigo-09 object-cover rounded"
+                />
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 flex-1">
+                  <InputField
+                    label="Nome completo"
+                    name="name"
+                    type="text"
+                    value={form.name}
+                  />
+                  <InputField
+                    label="CRM"
+                    name="crm"
+                    type="text"
+                    value={form.crm}
+                  />
+                </div>
+              </div>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6 flex-1">
+                <InputField
+                  label="Usuário"
+                  name="user"
+                  type="text"
+                  value={form.username}
+                />
+                <InputField
+                  label="Telefone"
+                  name="celphone"
+                  type="text"
+                  value={form.celphone}
+                />
+              </div>
+            </form>
           </div>
         </div>
       </section>
