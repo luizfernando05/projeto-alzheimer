4db6b7633ef343d310295af6b249eb09b822c03f
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-07-04 14:21:08 -0300

    ajustando a classe de cor do título no componente BlockMobilePage

--- root/frontend/src/Pages/DoctorPages/BlockMobilePage.jsx
+++ root/frontend/src/Pages/DoctorPages/BlockMobilePage.jsx
@@ -5,7 +5,7 @@ const BlockMobilePage = () => {
   return (
     <div className="h-screen flex flex-col gap-4 items-center justify-center text-center p-4">
       <LogoIcon />
-      <h1 className="text-xl text-red-600 font-semibold">
+      <h1 className="text-xl text-red-09 font-semibold">
         Esta página está disponível apenas para dispositivos desktop.
       </h1>
     </div>
