5e77fb9babed9b4c57c2d1389ee8008eeb0fd4d8
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-07-04 14:40:55 -0300

    ajustando a navegação ao clicar no botão de voltar nos componentes CreatePatient e CreateMedicalData

--- root/frontend/src/Pages/DoctorPages/PatientModulePages/CreateMedicalData.jsx
+++ root/frontend/src/Pages/DoctorPages/PatientModulePages/CreateMedicalData.jsx
@@ -139,7 +139,10 @@ const CreateMedicalData = () => {
           />
         )}
         <div className="bg-gray-01 rounded-xl shadow-sm border border-gray-06">
-          <div className="flex gap-6 pt-6 pb-6 pr-8 pl-8 border-b border-gray-06">
+          <div
+            onClick={() => navigate(-1)}
+            className="flex gap-6 pt-6 pb-6 pr-8 pl-8 border-b border-gray-06"
+          >
             <button className="flex items-center gap-2 text-xs text-gray-11 hover:text-gray-12 shadow-xs rounded-sm border border-gray-06 bg-gray-02 hover:bg-gray-03">
               <div className="pr-2 pl-2 pt-1 pb-1 border-r border-gray-06">
                 <ArrowLeft size={16} />
