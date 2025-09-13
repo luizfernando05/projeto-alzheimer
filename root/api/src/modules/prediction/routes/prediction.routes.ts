8803836f102f902df4561f1208150399a9cdfd5c
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-09-13 11:49:02 -0300

    adicionando middleware de autenticação para rota de criação de predição

--- root/api/src/modules/prediction/routes/prediction.routes.ts
+++ root/api/src/modules/prediction/routes/prediction.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { PredictionController } from '../controllers/PredictionController';
+import { ensureDoctorAuthenticated } from '../../shared/middlewares/ensureDoctorAuthenticated';
 
 const predictionRoutes = Router();
 const predictionController = new PredictionController();
 
-predictionRoutes.post('/', (req, res) => {
+predictionRoutes.post('/', ensureDoctorAuthenticated, (req, res) => {
   predictionController.create(req, res);
 });
 
