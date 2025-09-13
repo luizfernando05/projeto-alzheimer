a5ed25528caa4c192724668d5680e98d8111f397
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-09-13 11:45:41 -0300

    adicionando axios como dependência e configurando rotas de predição

--- root/api/src/infra/http/routes/routes.ts
+++ root/api/src/infra/http/routes/routes.ts
@@ -3,6 +3,7 @@ import { adminRoutes } from '../../../modules/admin/routes/admin.routes';
 import { doctorRoutes } from '../../../modules/doctor/routes/doctor.routes';
 import { patientRoutes } from '../../../modules/patient/routes/patient.routes';
 import { medicalDataRoutes } from '../../../modules/medicalData/routes/medicalData.routes';
+import { predictionRoutes } from '../../../modules/prediction/routes/prediction.routes';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use('/admin', adminRoutes);
 routes.use('/doctor', doctorRoutes);
 routes.use('/patient', patientRoutes);
 routes.use('/data', medicalDataRoutes);
+routes.use('/predict', predictionRoutes);
 
 export { routes };
